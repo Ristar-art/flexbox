4c553ee21f6539d9d0283301225f41e4a9885b6b
mochochokoboiketlo <mochochokoboiketlo@gmail.com>
2023-06-06 17:49:18 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div class="flexbox-conteiner">
+          <div class="flexbox-item1 " >
+          </div>
+          <div class="flexbox-item1 ">
+          </div>
+          <div class="flexbox-item1 ">
+      </div>
+
+     </div>  
+     <div class="flexbox-conteiner">
+          <div class="flexbox-item2">
+          </div>
+          <div class="flexbox-item3">
+          </div>
+          <div class="flexbox-item2">
+      </div>
+
+
+     </div>  
+     <div class="flexbox-conteiner">
+          <div class="flexbox-items4">
+          </div>  
+     </div>  
+     
+     </div>   
+                   
+       
+      
+   
+  );
+}
+
+export default App;
