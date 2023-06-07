8baff248d7f70b48735f86582bf04f8bb28efc54
mochochokoboiketlo <mochochokoboiketlo@gmail.com>
2023-06-07 14:56:53 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -4,31 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
         <div class="flexbox-conteiner">
-          <div class="flexbox-item1 " >
+          <div class="flexbox-item flexbox-item1 " >
           </div>
-          <div class="flexbox-item1 ">
+          <div class="flexbox-item flexbox-item2 " >
           </div>
-          <div class="flexbox-item1 ">
-      </div>
-
-     </div>  
-     <div class="flexbox-conteiner">
-          <div class="flexbox-item2">
+          <div class="flexbox-item flexbox-item1 " >
           </div>
-          <div class="flexbox-item3">
-          </div>
-          <div class="flexbox-item2">
-      </div>
-
-
-     </div>  
-     <div class="flexbox-conteiner">
-          <div class="flexbox-items4">
+         
+        </div>  
+        <div class="flexbox-conteiner">
+            <div class="flexbox-item flexbox-item3 " >
+              </div>
+            <div class="flexbox-item flexbox-item4 " >
+              </div>
+            <div class="flexbox-item flexbox-item3 " >
+              </div>
+         </div>  
+         <div class="flexbox-conteiner">
+             <div class="flexbox-item flexbox-item1" >
+               </div>
           </div>  
-     </div>  
-     
-     </div>   
+         
+    </div>   
                    
        
       
